b73d3110ea4979fc5a02a74f7eef070dc1f802c0
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-05-03 12:48:13 +0200

    keep expanding test

--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,33 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-use std::ffi::CString;
+use std::ffi::{CStr, CString};
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_init() {
+    fn test_probe() {
         unsafe {
+            tbstats_reset();
+            tbcache_restart(1024 * 1024, 50);
+
             let paths = tbpaths_init();
             let paths = tbpaths_add(paths, CString::new("/home/niklas/Projekte/syzygy-tables.info/gaviota").unwrap().as_ptr());
 
             let initinfo = tb_init(1, TB_compression_scheme_tb_CP4 as i32, paths);
+            if initinfo.is_null() {
+                panic!("tb_init failed");
+            }
+            assert!(tb_is_initialized() != 0);
+            println!("{:?}", CStr::from_ptr(initinfo));
+            println!("availability: {}", tb_availability());
+
+            tbpaths_done(paths);
+            tb_done();
+
+            panic!("intended fail");
         }
     }
 }
