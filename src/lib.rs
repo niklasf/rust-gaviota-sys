2d2ad39401cdc546659c80a6d18402e50d910435
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-09-11 13:13:10 +0200

    prepare 0.1.2

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@
 // IN THE SOFTWARE.
 
 
-#![doc(html_root_url = "https://docs.rs/gaviota-sys/0.1.1")]
+#![doc(html_root_url = "https://docs.rs/gaviota-sys/0.1.2")]
 
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
