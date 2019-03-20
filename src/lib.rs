a9246fb7fd7590e5b2576b672efb740264311fc2
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-03-20 21:12:17 +0100

    bump 0.1.7

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@
 // IN THE SOFTWARE.
 
 
-#![doc(html_root_url = "https://docs.rs/gaviota-sys/0.1.6")]
+#![doc(html_root_url = "https://docs.rs/gaviota-sys/0.1.7")]
 
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
