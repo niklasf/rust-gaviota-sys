fce354f6a5d10708e5f3b75fea6d17985694de1d
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-05-22 10:46:42 +0200

    bump 0.1.13 (bindgen 0.54)

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@
 // IN THE SOFTWARE.
 
 
-#![doc(html_root_url = "https://docs.rs/gaviota-sys/0.1.12")]
+#![doc(html_root_url = "https://docs.rs/gaviota-sys/0.1.13")]
 
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
