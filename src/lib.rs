3154bc9fbd5f92454f5551ffb23a7d5bb71ca529
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-08-19 10:16:32 +0200

    forbid unsafe_op_in_unsafe_fn

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,7 @@
 
 #![doc(html_root_url = "https://docs.rs/gaviota-sys/0.1.17")]
 
+#![forbid(unsafe_op_in_unsafe_fn)]
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
