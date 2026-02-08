04c5cf947ad5a424aecb42e3641b3433f6d1b285
Niklas Fiekas <niklas.fiekas@backscattering.de>
2026-02-08 12:57:09 +0100

    update lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
-#![forbid(unsafe_op_in_unsafe_fn)]
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
+#![allow(nonstandard_style)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
